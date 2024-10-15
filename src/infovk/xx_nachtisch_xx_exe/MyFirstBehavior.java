2f9d7a1f5df690eedfa628521d34892e7bcdc4ae
Maxis7164 <maxis7164@gmail.com>
2024-10-15 12:06:51 +0200

    implemented gun turning

--- src/infovk/xx_nachtisch_xx_exe/MyFirstBehavior.java
+++ src/infovk/xx_nachtisch_xx_exe/MyFirstBehavior.java
@@ -1,12 +1,23 @@
 package infovk.xx_nachtisch_xx_exe;
 
 import java.awt.*;
+import java.lang.reflect.Array;
 
 import static infovk.xx_nachtisch_xx_exe.Utils.*;
 import static infovk.xx_nachtisch_xx_exe.Wrappers.*;
 
-
 public class MyFirstBehavior extends SimpleRobotBehavior {
+	double STEP = 0.05;
+	double SIN_MOD = 40;
+
+	int GAP = 36;
+
+	int SIZE_X = 800;
+	int SIZE_Y = 600;
+	
+	double t = -1; // t-value [-1; 1]
+	double dir = 1; // determines turning direction
+	
 	public MyFirstBehavior(SimpleRobot  robot) {
 		super(robot);
 	}
@@ -19,16 +30,45 @@ public class MyFirstBehavior extends SimpleRobotBehavior {
 	@Override
 	void execute() {
 		scan();
-
+		drive();
 		shoot();
 	}
 
+	
+	//#region utils
 	double getEnemyAngle(ScannedRobotEvent e, double heading) {
 		double own = getHeading() - heading;
 		double enemy = e.getBearing();
 
 		return normalRelativeAngle((own + enemy)) * 1.1;
 	}
+
+	boolean heading(int dir) {
+		if (dir < 0 || dir > 3) return false;
+		double head = getHeading();
+
+		if (dir == 0) return head > 315 || head < 45;
+		else if (dir == 1) return head > 45 && head < 135;
+		else if (dir == 2) return head > 135 && head < 225;
+		else return head > 225 && head < 315;
+	}
+
+	void continueT() {
+		t += dir * STEP;
+		if (t >= 1) dir = -1;
+		else if (t <= -1) dir = 1;
+	}
+
+	int avoidWalls() {
+		double x = getX();
+		double y = getY();
+
+		return heading(0) && y >= 559 ||
+		heading(2) && y <= 41 ||
+		heading(1) && x >= 759 ||
+		heading(3) && x <= 41 ? -1 : 1;
+	}
+	//#endregion
 
 	void scan() {
 		if (!hasScannedRobot()) return;
@@ -39,6 +79,16 @@ public class MyFirstBehavior extends SimpleRobotBehavior {
 
 		turnRadar(lastEnemyAngle);
 	}
+
+	void drive() {
+		turn(Math.sin(t) * SIN_MOD);
+
+		int dir = avoidWalls();
+
+		ahead(dir * 50);
+		continueT();
+	}
+
 
 	void shoot() {
 		if (getGunHeat() > 0 || !hasScannedRobot()) return;
