25920cc2610cc5cb0920f7ab39684a35dce7dcfa
Maxis7164 <maxis7164@gmail.com>
2024-10-15 11:07:15 +0200

    implemented gun turning

--- src/infovk/xx_nachtisch_xx_exe/MyFirstBehavior.java
+++ src/infovk/xx_nachtisch_xx_exe/MyFirstBehavior.java
@@ -7,8 +7,6 @@ import static infovk.xx_nachtisch_xx_exe.Wrappers.*;
 
 
 public class MyFirstBehavior extends SimpleRobotBehavior {
-	double lastEnemyAngle = 0;
-
 	public MyFirstBehavior(SimpleRobot  robot) {
 		super(robot);
 	}
@@ -25,24 +23,33 @@ public class MyFirstBehavior extends SimpleRobotBehavior {
 		shoot();
 	}
 
+	double getEnemyAngle(ScannedRobotEvent e, double heading) {
+		double own = getHeading() - heading;
+		double enemy = e.getBearing();
+
+		return normalRelativeAngle((own + enemy)) * 1.1;
+	}
+
 	void scan() {
 		if (!hasScannedRobot()) return;
 
 		ScannedRobotEvent e = getScannedRobotEvent();
 
-		double ownAngle = this.getHeading() - this.getRadarHeading();
-		double enemyAngle = e.getBearing();
-
-		lastEnemyAngle = normalRelativeAngle((ownAngle + enemyAngle)) * 1.1;
+		double lastEnemyAngle = getEnemyAngle(e, getRadarHeading());
 
 		turnRadar(lastEnemyAngle);
 	}
 
 	void shoot() {
-		if (this.getGunHeat() > 0) return;
+		if (getGunHeat() > 0 || !hasScannedRobot()) return;
 
+		ScannedRobotEvent e = getScannedRobotEvent();
 
-		turnGun(lastEnemyAngle);
-		fireBullet(1);
+		double gunAngle = getEnemyAngle(e, getGunHeading());
+
+		turnGun(gunAngle);
+
+		fireBullet(0.1);
+
 	}
 }
